f9cf3b098a354680385c3a5a0ad3fde4077a8839
ACardenasSil <alsillasedu@gmail.com>
2022-11-22 13:40:31 -0800

    Completed perceptron. Believed to be functional.

--- main.cpp
+++ main.cpp
@@ -15,8 +15,8 @@ template <size_t rows, size_t cols>
 void readCSV(double(&array)[rows][cols], char* filename) {
 	std::ifstream file(filename);
     
-	std::string format;
-	std::getline(file, format); //get rid of the first line
+	std::string temp;
+	std::getline(file, temp); //get rid of the first line
 
 	for (size_t row = 0; row < rows; ++row)
 	{
@@ -61,15 +61,17 @@ void printStatus(int iteration, double (&weights)[w_i], double error, int correc
     std::cout << ": Weights are [";
     for ( int i = 0; i < w_i; i++) {
         std::cout << weights[i];
+        std::cout << ", ";
     }
     std::cout << "], classification was ";
-    if (error > 0.1 || error < -0.1) { //floating point fix
+    if (error > 0.001 || error < -0.001) { //floating point fix
         std::cout << "ERROR!";
     } else {
         std::cout << "CORRECT!";
     }
+    //std::cout << std::endl;
+    //std::cout << "Correct " << correct/150.0 *100.0 << "%." << std::endl;
     std::cout << std::endl;
-    std::cout << "Correct " << correct << "out of 150" << std::endl;
 }
 
 template <size_t w_i, size_t x_i>
@@ -80,17 +82,18 @@ double updateWeights(double (&weights)[w_i], double alpha, double (&input)[x_i],
 }
 
 template <size_t attr_rows, size_t x_i, size_t w_i>
-void perceptron(double(&attributes)[attr_rows][x_i], double(&weights)[w_i], double theta, double alpha) {
+int perceptron(double(&attributes)[attr_rows][x_i], double(&weights)[w_i], double theta, double alpha) {
     // for loop which classifies each element in array using if statement
     //iterations 
     int maxCorrect = 0;
-    int error=0;
+    int iterations = 0;
+    double perCorrect = 0.0;
+    int error=0; 
     for (int i = 0; i < attr_rows; i++) {
+        iterations += 1;
         double isIris = 0;
         double inputs[x_i-1];
-        double sum[attr_rows];
-        //std::array<int,4> A = {10,20,30,40};
-        //std::array<int,4> B = A;
+        double sum[attr_rows] = {};
         // values and weights
         for (int j = 0; j < x_i-1; j++) {
             inputs[j] = attributes[i][j];
@@ -99,14 +102,29 @@ void perceptron(double(&attributes)[attr_rows][x_i], double(&weights)[w_i], doub
         }
         
         // if true, then Iris Virginica else NOT Iris
-        isIris = (sum[i] > theta) ? 1.0 : -1.0;
+        double activation = sum[i] - theta;
+        isIris = (activation >= 0.0) ? 1.0 : 0.0;
         
         //error will be either 1 or 0
-        double output = attributes[i][4] - isIris;
-        if (output > 0.1 || output < -0.1) {error = 1;} else {error = 0; maxCorrect++;}
-        printStatus(i, weights, error, maxCorrect);
+        //get attributes to be 0 if -1 or 1 if 1 using math
+        double output = (attributes[i][4]+1)/2  - isIris;
+        //std::cout << (attributes[i][4]+1)/2 << "-" << isIris <<std::endl;
+        if (output > 0.1) {
+            error = 1;
+        } 
+        else if (output < -0.1) {
+            error = -1;
+        } else {
+            error = 0; 
+            maxCorrect++;
+        }
+        printStatus(iterations, weights, error, maxCorrect);
         updateWeights(weights, alpha, inputs, error);
-    }
+    } 
+    perCorrect = maxCorrect / (iterations*1.0) * 100.0;
+
+    std::cout << "percentCorrect: %" << perCorrect << std::endl;
+    return maxCorrect;
     
 }
 
@@ -122,14 +140,14 @@ int main(int argc, char* argv[])
     // [0-151:line][0:SepalLen, 1:SepalWid, 2:PetalLen, 3:PetalWid, 4:Species]
     double data[NUM_SAMPLES][5]; 
     double label; // Iris Virginica = 1 // NOT Iris Virginica = -1 
-    double threshold; 
+    double threshold = 1; //? 
     double learning = 0.05; 
-
+    int correct = 0;
     // set weights to random numbers from -0.5 to 0.5
     double weights[4] = {init(), init(), init(), init()}; 
     double predicted_labels[NUM_SAMPLES][2]; 
 
-    double thresholdResult_pair[2]; //[threshold, percent_correct]
+    double thresholdResult_pair[2] = {0,0}; //[threshold, percent_correct]
 
 
     // Use readCSV to create data array
@@ -138,13 +156,15 @@ int main(int argc, char* argv[])
     std::cout << "\n---ground truths---\n" << std::endl;
     displayValues(data);
     /*
-    for (threshold = 0.0; threshold < 10; threshold += 0.1) {
+    for (threshold = 0.0; threshold < 7; threshold += 0.1) {
         double correct = 0.0;
         double percent_correct = 0.0;
 
         // for loop which classifies each element in array using if statement
         */
-        perceptron(data, weights, threshold, learning);
+        
+        correct = perceptron(data, weights, threshold, learning);
+        
         /*
         //std::cout << "\n---predicted labels---\n" << std::endl;
         //displayValues(predicted_labels);
@@ -161,15 +181,16 @@ int main(int argc, char* argv[])
         std::cout << "\n---\ncorrect: " << correct << std::endl;
         percent_correct = (correct / NUM_SAMPLES) * 100; 
         std::cout << percent_correct << "\% correct" << std::endl;
+        
 
         if (thresholdResult_pair[1] < correct) {
             thresholdResult_pair[1] = correct;
             thresholdResult_pair[0] = threshold;
         }
-        std::cout << out << std::endl;
         std::cout << "\n---\nMost correct: "<< thresholdResult_pair[1] << std::endl;
         std::cout << "\n---\nBest threshold: "<< thresholdResult_pair[0] << std::endl;
-    } 
-    */
+        std::cout << std::endl;
+    //} 
+    //*/
     return 0;
 }
